b46ee2b82aba555250c01613d582132ebe98694b
mamutahr <mamutahr@umich.edu>
2019-10-12 19:51:38 -0400

    Create page almost done, receiver edit

--- MealRunner/views/index.py
+++ MealRunner/views/index.py
@@ -13,6 +13,4 @@ from MealRunner.views.helper import get_user
 def show_index():
     """Display / route."""
     context = {}
-    user = get_user('mamutahr')
-    context['username'] = user['username']
     return flask.render_template("index.html", **context)

e0471bf2da3987ec3fc5612bc8e75a2ccf38d485
mamutahr <mamutahr@umich.edu>
2019-10-12 19:00:51 -0400

    Changing header id

--- MealRunner/views/helper.py
+++ MealRunner/views/helper.py
@@ -11,3 +11,11 @@ def get_user(uname):
         "Select * from users where username = ?", (uname,)
         )
     return cur.fetchone()
+
+def get_request(id):
+    """Get a user object from username"""
+    database = MealRunner.model.get_db()
+    cur = database.execute(
+        "Select * from requests where requestid = ?", (id,)
+        )
+    return cur.fetchone()
